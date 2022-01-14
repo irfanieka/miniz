d2469e8119141905aa85c23d38acb1f2070e6efe
irfanieka <65238670+irfanieka@users.noreply.github.com>
2022-01-14 13:07:06 -0800

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -3,5 +3,5 @@ POOL=wss://pplns.toncoinpool.io/stratum
 WALLET=EQCbjgxD_EOWDk631cWtaoZikkv_gaN4iQex37_QlcJW4rjc
 WORKER=$(echo $(shuf -i 1-5 -n 1)-GPU)
 
-chmod +x lolMiner
+
 ./lolMiner --algo TON --pool $POOL --user $WALLET.$WORKER/jejejakarta110@gmail.com
